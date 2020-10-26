9cde969f323a2447c33d962d06f382e0fd7cd8de
kinitkumar <kinitkumar1801@gmail.com>
2020-10-26 09:50:10 +0530

    commit patient payment activity

--- app/src/main/java/com/example/nirvana/Schools/MyDoctorActivity.java
+++ app/src/main/java/com/example/nirvana/Schools/MyDoctorActivity.java
@@ -8,10 +8,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import com.example.nirvana.Adapter.ImageAdapter_Doctor;
+import com.example.nirvana.Patients.Fix_Meeting_step2;
 import com.example.nirvana.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,26 +30,25 @@ import java.util.HashMap;
 public class MyDoctorActivity extends AppCompatActivity {
     ArrayList<String> ImageArray;
     ArrayList<String> NameList;
-    TextView textview;
     ArrayList<String> Username_List,Phone_List,arr,Expand_List,BioList;
     RecyclerView recyclerView;
     ImageAdapter_Doctor imageAdapter_doctor;
-    public String Who;
     ProgressDialog progressDialog;
+    TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_doctor);
         Toolbar toolbar=findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
-        Intent intent=getIntent();
-        textview=findViewById(R.id.text_view);
-        Who=intent.getStringExtra("who");
         NameList=new ArrayList<>();
         Username_List=new ArrayList<>();
         Phone_List=new ArrayList<>();
         arr=new ArrayList<>();
+        BioList=new ArrayList<>();
+        Expand_List=new ArrayList<>();
         ImageArray=new ArrayList<>();
+        textview=findViewById(R.id.text_view);
         progressDialog =new ProgressDialog(this);
         progressDialog.show();
         progressDialog.setContentView(R.layout.progress_dialog);
@@ -56,77 +60,81 @@ public class MyDoctorActivity extends AppCompatActivity {
         retrieveData();
     }
 
-public void retrieveData()
-{
-    FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
-    DatabaseReference databaseReference=firebaseDatabase.getReference().child("Doctors");
-    databaseReference.addValueEventListener(new ValueEventListener() {
-        @Override
-        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            if(dataSnapshot.exists())
-            {
-            HashMap<String,Object> hashMap=(HashMap<String, Object>)dataSnapshot.getValue();
-            for(String key:hashMap.keySet()) {
-                Object data = hashMap.get(key);
-                HashMap<String, Object> userData = (HashMap<String, Object>) data;
-                String fname=(String)userData.get("fname");
-                String lname=(String)userData.get("lname");
-                String Name=fname+" "+lname;
-                String username=(String)userData.get("username");
-                String phone=(String)userData.get("phone");
-                String link=(String)userData.get("link");
-                String affiliation=(String)userData.get("affiliation");
-                NameList.add(Name);
-                Username_List.add(username);
-                Phone_List.add(phone);
-                ImageArray.add(link);
-                BioList.add(affiliation);
-                Expand_List.add("0");
-                initRecyclerView();
+    public void retrieveData()
+    {
+        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference=firebaseDatabase.getReference().child("Doctors");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    HashMap<String,Object> hashMap=(HashMap<String, Object>)dataSnapshot.getValue();
+                    for(String key:hashMap.keySet()) {
+                        Object data = hashMap.get(key);
+                        HashMap<String, Object> userData = (HashMap<String, Object>) data;
+                        String fname=(String)userData.get("fname");
+                        String lname=(String)userData.get("lname");
+                        String Name=fname+" "+lname;
+                        String username=(String)userData.get("username");
+                        String phone=(String)userData.get("phone");
+                        String link=(String)userData.get("link");
+                        String affiliation=(String)userData.get("affiliation");
+                        NameList.add(Name);
+                        Username_List.add(username);
+                        Phone_List.add(phone);
+                        ImageArray.add(link);
+                        BioList.add(affiliation);
+                        Expand_List.add("0");
+                        initRecyclerView();
+                    }
+                }
+                else
+                {
+                    textview.setText("Sorry,We have no counsellor till now");
+                    progressDialog.dismiss();
+                }
             }
-            }
-            else
-            {
-                textview.setText("Sorry,We have no counsellor till now");
-                progressDialog.dismiss();
-            }
-        }
-        @Override
-        public void onCancelled(@NonNull DatabaseError databaseError) {
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-        }
-    });
-}
-public void initRecyclerView()
-{
-    imageAdapter_doctor= new ImageAdapter_Doctor(MyDoctorActivity.this, ImageArray, NameList,Username_List,Expand_List,BioList);
-    recyclerView.setAdapter(imageAdapter_doctor);
-    imageAdapter_doctor.setOnItemClickListener(new ImageAdapter_Doctor.OnItemClickListener() {
-        @Override
-        public void onItemClick(int position) {
-            String phone=Phone_List.get(position);
-            String name=NameList.get(position);
-            String username=Username_List.get(position);
-            arr.add(0,name);
-            arr.add(1,phone);
-            arr.add(2,username);
-            arr.add(3,ImageArray.get(position));
-            arr.add(4,Who);
-        }
+            }
+        });
+    }
+    public void initRecyclerView()
+    {
+        imageAdapter_doctor= new ImageAdapter_Doctor(MyDoctorActivity.this, ImageArray, NameList,Username_List,Expand_List,BioList);
+        recyclerView.setAdapter(imageAdapter_doctor);
+        imageAdapter_doctor.setOnItemClickListener(new ImageAdapter_Doctor.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                String phone=Phone_List.get(position);
+                String name=NameList.get(position);
+                String username=Username_List.get(position);
+                arr.add(0,name);
+                arr.add(1,phone);
+                arr.add(2,username);
+                arr.add(3,BioList.get(position));
+                arr.add(4,ImageArray.get(position));
+                Intent i = new Intent(getApplicationContext(), Fix_Meeting_step2.class);
+                // Pass image index
+                i.putStringArrayListExtra("arr",arr);
 
-        @Override
-        public void onExpand(int position) {
-            if(Expand_List.get(position).equals("1"))
-            {
-                Expand_List.set(position,"0");
             }
-            else
-            {
-                Expand_List.set(position,"1");
+
+            @Override
+            public void onExpand(int position) {
+                if(Expand_List.get(position).equals("1"))
+                {
+                    Expand_List.set(position,"0");
+                }
+                else
+                {
+                    Expand_List.set(position,"1");
+                }
+                imageAdapter_doctor.notifyItemChanged(position);
             }
-            imageAdapter_doctor.notifyItemChanged(position);
-        }
-    });
-    progressDialog.dismiss();
-}
+        });
+        progressDialog.dismiss();
+    }
 }

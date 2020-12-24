39d6c59fa4a72bdf7e2aabc813955f71c7b4a0ec
Shreyashi <shreyashikumari2019@gmail.com>
2020-12-24 17:37:55 +0530

    Front end Updation

---
+++ app/src/main/java/com/example/nirvana/Doctors/activity_doctor_profile.java
@@ -0,0 +1,16 @@
+package com.example.nirvana.Doctors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nirvana.R;
+
+public class activity_doctor_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+    }
+}
--- app/src/main/java/com/example/nirvana/Patients/PatientSignupActivity.java
+++ app/src/main/java/com/example/nirvana/Patients/PatientSignupActivity.java
@@ -32,7 +32,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PatientSignupActivity extends AppCompatActivity {
-    String Age,Email,Pass,Phone,Address,Gender,Fname,Lname,Password;
+    String Pass,Fname,Password;
     private Spinner spinner;
     private ArrayList<String> arr;
     @Override
@@ -53,60 +53,26 @@ public class PatientSignupActivity extends AppCompatActivity {
                 + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
     }
     public void register_patient(View view) {
-        EditText age,password,email,confirm_password,phone,address,fname,lname;
-        RadioGroup gender;
-        RadioButton patient_gender,male,female;
+        EditText password,confirm_password,phone,fname;
         final ProgressBar progressBar;
-        age=findViewById(R.id.age_patient);
-        fname=findViewById(R.id.fname_patient);
-        lname=findViewById(R.id.lname_patient);
-        email=findViewById(R.id.email_patient);
+        fname=findViewById(R.id.name_patient);
         phone=findViewById(R.id.mob_patient);
-        address=findViewById(R.id.address_patient);
         password=findViewById(R.id.password1_patient);
         confirm_password=findViewById(R.id.password2_patient);
         progressBar=findViewById(R.id.progressBar2_patient);
         progressBar.setVisibility(View.VISIBLE);
-        gender=findViewById(R.id.patient_gender);
         String code = CountryCode.countryAreaCodes[spinner.getSelectedItemPosition()];
-        int selectedId=gender.getCheckedRadioButtonId();
-        patient_gender=findViewById(selectedId);
         String pw=password.getText().toString().trim();
         String cpw=confirm_password.getText().toString().trim();
-        if(TextUtils.isEmpty(age.getText()))
-        {
-            age.setError("Please enter the age");
-            progressBar.setVisibility(View.GONE);
-        }
 
-        else if(TextUtils.isEmpty(fname.getText()))
+        if(TextUtils.isEmpty(fname.getText()))
         {
             fname.setError("Please enter the first name");
-            progressBar.setVisibility(View.GONE);
-        }
-        else if(TextUtils.isEmpty(lname.getText()))
-        {
-            lname.setError("Please enter the last name");
-            progressBar.setVisibility(View.GONE);
-        }
-        else if(TextUtils.isEmpty(email.getText()))
-        {
-            email.setError("Please enter the email");
-            progressBar.setVisibility(View.GONE);
-        }
-        else if(!(isValidEmailId(email.getText().toString())))
-        {
-            email.setError("Please enter the valid email");
             progressBar.setVisibility(View.GONE);
         }
         else if(TextUtils.isEmpty(phone.getText()))
         {
             phone.setError("Please enter the phone number");
-            progressBar.setVisibility(View.GONE);
-        }
-        else if(TextUtils.isEmpty(address.getText()))
-        {
-            address.setError("Please enter the city and state");
             progressBar.setVisibility(View.GONE);
         }
         else if(TextUtils.isEmpty(password.getText()))
@@ -132,39 +98,21 @@ public class PatientSignupActivity extends AppCompatActivity {
             password.setError("Password do not matched");
             progressBar.setVisibility(View.GONE);
         }
-        else if(gender.getCheckedRadioButtonId()==-1)
-        {
-            Toast.makeText(this,"Please select the gender",Toast.LENGTH_SHORT).show();
-            progressBar.setVisibility(View.GONE);
-        }
         else
         {
-            String email1 = email.getText().toString().trim();
+
             String phone1 = phone.getText().toString().trim();
-            String address1 = address.getText().toString().trim();
-            String age1 = age.getText().toString().trim();
-            String gender1 = patient_gender.getText().toString().trim();
             String fname1 = fname.getText().toString().trim();
-            String lname1 = lname.getText().toString().trim();
             String pass1=password.getText().toString();
             String phonenumber = "+" + code + phone1;
-            Age=age1;
-            Email=email1;
             Phone=phonenumber;
-            Address=address1;
-            Gender=gender1;
             Fname=fname1;
-            Lname=lname1;
             Pass=pass1;
             arr=new ArrayList<String>();
-            arr.add(0,Age);
-            arr.add(1,Email);
-            arr.add(2,Phone);
-            arr.add(3,Address);
-            arr.add(4,Gender);
-            arr.add(5,Fname);
-            arr.add(6,Lname);
-            arr.add(7,Password);
+            arr.add(0,Phone1);
+            arr.add(1,Address);
+            arr.add(2,Fname);
+            arr.add(3,Password);
 
             FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
             DatabaseReference databaseReference=firebaseDatabase.getReference().child("Patients");

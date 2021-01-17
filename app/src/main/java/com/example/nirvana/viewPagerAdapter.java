e4c3fc05c2b34309ddfdbe05d4ef94e06289789a
Shreyashi <shreyashikumari2019@gmail.com>
2021-01-17 17:26:12 +0530

    Created my_meetings, upcomming_meetings and history fragment

---
+++ app/src/main/java/com/example/nirvana/viewPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.nirvana;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+import static android.media.CamcorderProfile.get;
+import static okhttp3.internal.tls.CertificateChainCleaner.get;
+
+public class viewPagerAdapter extends FragmentPagerAdapter {
+
+    private final ArrayList<Fragment> fragmentslist= new ArrayList<>();
+    private final ArrayList<String> fragmentTitle = new ArrayList<>();
+
+
+    public viewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentslist.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentslist.size();
+    }
+
+    public void addFragment (Fragment fragment, String title){
+        fragmentslist.add(fragment);
+        fragmentTitle.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+}

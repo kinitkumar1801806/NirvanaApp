94cbf35c4c75fd27326674abc5b7cc1939fd36da
Shreyashi <shreyashikumari2019@gmail.com>
2021-01-30 00:23:06 +0530

    pages added

---
+++ app/src/main/java/com/example/nirvana/Messages_Alerts.java
@@ -0,0 +1,14 @@
+package com.example.nirvana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Messages_Alerts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meetings__alerts);
+    }
+}
